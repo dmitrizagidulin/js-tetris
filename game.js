5ecb0f1ff7d62165a2cbfcab24870e856e716882
dzagidulin <dmitri@zagidulin.net>
2012-02-04 12:41:48 -0500

    Draw a grid in the game area

--- game.js
+++ game.js
@@ -24,20 +24,65 @@
 	function GameState() {
 		var square_side = 20
 		var square_color = "green"
-		
+		var jc = jaws.context
+			
 		this.setup = function() {
 		}
 		
 		this.draw = function() {
+			jaws.clear()
+			
+//			jc.lineWidth = 2
+//			jc.strokeStyle = "black"
+//			jc.beginPath()
+//			jc.moveTo(10,10)
+//			jc.lineTo(10, 20)
+//			jc.closePath()
+//			jc.stroke()
+			
 			// Clear screen
-			jaws.context.clearRect(0,0,jaws.width,jaws.height)
+			jaws.context.fillStyle = "black"
+			jaws.context.fillRect(0,0,jaws.width,jaws.height)
+			
+			// Draw game area
+			jaws.context.strokeStyle = "white"
+			jaws.context.lineWidth = 1
+			var game_area_height = square_side * 20
+			var game_area_width = square_side * 10
+			var game_area_x = 50
+			var game_area_y = 20
+			jaws.context.strokeRect(game_area_x, game_area_y, game_area_width, game_area_height)
+
+			var line_height = 0
+
+			jc.lineWidth = 1
+			jc.strokeStyle = "white"
+			
+			// draw horizontal lines of grid
+			for(var i=1; i < 20; i++) {
+				line_height = game_area_y + (i * square_side)
+				jc.beginPath()
+				jc.moveTo(game_area_x, line_height)
+				jc.lineTo(game_area_width + game_area_x, line_height)
+				jc.stroke()
+				jc.closePath()
+			}
+			
+			// Draw vertical lines of grid
+			for(var i=1; i < 10; i++) {
+				line_width = game_area_x + (i * square_side)
+				jc.beginPath()
+				jc.moveTo(line_width, game_area_y)
+				jc.lineTo(line_width, game_area_y + game_area_height)
+				jc.stroke()
+			}
 			
 			// Ok, let's draw one rectangle
 			jaws.context.strokeStyle = "black"
 			jaws.context.fillStyle = square_color
 			jaws.context.lineWidth = 3
-			var x = 40
-			var y = 40
+			var x = game_area_x + 40
+			var y = game_area_y + 40
 			jaws.context.strokeRect(x, y, square_side, square_side)
 			jaws.context.fillRect(x, y, square_side, square_side)
 		}
@@ -79,5 +124,5 @@
 *
 */
 	window.onload = function() {
-		jaws.start(MenuState)
+		jaws.start(GameState)
 	}
